6499ec1bd7f5caf8b8ba12a06fdf85bcf56ddbca
code-death <poorvanshkavta938@gmial.com>
2022-07-02 23:32:07 +0530

    Added context and updating items number in cart using it

--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,14 +1,22 @@
 import classes from './HeaderCardButton.module.css'
 import CartIcon from '../Cart/CartIcon'
+import { useContext } from 'react'
+import CartContext from '../../Store/CartContext'
 
 const HeaderCardButton = props => {
+  const cartCtx = useContext(CartContext)
+
+  const numberOfCartItems = cartCtx.items.reduce((cur, item) => {
+    return cur + item.amount
+  }, 0)
+
   return (
     <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
