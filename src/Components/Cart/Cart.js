42c635c21544fd37c68b92ada8969037b1fbbf37
code-death <poorvanshkavta938@gmial.com>
2022-07-03 10:50:51 +0530

    Added adding removing functionality in the cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,8 +10,12 @@ const Cart = props => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
   const hasItems = cartCtx.items.length > 0
 
-  const cartItemRemoveHandler = () => {}
-  const cartItemAddHandler = () => {}
+  const cartItemRemoveHandler = id => {
+    cartCtx.removeItem(id)
+  }
+  const cartItemAddHandler = item => {
+    cartCtx.addItem({ ...item, amount: 1 })
+  }
 
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -23,7 +27,7 @@ const Cart = props => {
             amount={item.amount}
             price={item.price}
             onRemove={cartItemRemoveHandler.bind(null, item.id)}
-            onAdd={cartItemAddHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
           />
         )
       })}
