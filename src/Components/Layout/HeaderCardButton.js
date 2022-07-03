f9d03b463b2e81b5b7daff83c19504b7e58122c3
code-death <poorvanshkavta938@gmial.com>
2022-07-03 11:02:09 +0530

    Finished the project

--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,17 +1,33 @@
 import classes from './HeaderCardButton.module.css'
 import CartIcon from '../Cart/CartIcon'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CartContext from '../../Store/CartContext'
 
 const HeaderCardButton = props => {
   const cartCtx = useContext(CartContext)
-
-  const numberOfCartItems = cartCtx.items.reduce((cur, item) => {
+  const { items } = cartCtx
+  const [btnAnimated, setBtnAnimated] = useState(false)
+  const numberOfCartItems = items.reduce((cur, item) => {
     return cur + item.amount
   }, 0)
+  const btnClasses = `${classes.button} ${btnAnimated ? classes.bump : ''}`
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnAnimated(true)
+
+    const timer = setTimeout(() => {
+      setBtnAnimated(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
 
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
